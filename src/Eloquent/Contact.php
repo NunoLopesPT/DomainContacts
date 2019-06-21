5ef0185a66f6b01f61b24b2b75c26a00daff7629
Nuno Lopes <nuno@redweb.dk>
2019-06-21 14:01:05 +0100

    Created Eloquent Models for Contact and User

---
+++ src/Eloquent/Contact.php
@@ -0,0 +1,42 @@
+<?php
+namespace NunoLopes\LaravelContactsAPI\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Contact Eloquent's Model class
+ *
+ * This class will be used for communicating with the database's 'contacts' table.
+ *
+ * @property int    id           - Id of the contact.
+ * @property int    user_id      - Id of the user who created the contact.
+ * @property string first_name   - First name of the contact.
+ * @property string last_name    - Last name of the contact.
+ * @property string email        - Email of the contact.
+ * @property string phone_number - Phone number of the contact.
+ */
+class Contact extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array $fillable
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+    ];
+
+    /**
+     * Will return the User of the Contact.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ src/Eloquent/User.php
@@ -0,0 +1,57 @@
+<?php
+namespace NunoLopes\LaravelContactsAPI\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * User Eloquent's Model class
+ *
+ * This class will be used for communicating with the database's 'user' table.
+ *
+ * @property int    id    - Id of the User.
+ * @property string name  - Name of the User.
+ * @property string email - Email of the User.
+ */
+class User extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Will return the contacts of the User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
+}
