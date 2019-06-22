92bcf0c61422696ccc7fe73536f75c45ef89e783
Nuno Lopes <nuno@redweb.dk>
2019-06-22 19:26:56 +0100

    Improved AbstractValidationRequest

--- src/Requests/AbstractValidationRequest.php
+++ src/Requests/AbstractValidationRequest.php
@@ -5,18 +5,35 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Translation\FileLoader;
 use Illuminate\Translation\Translator;
 use Illuminate\Validation\Validator;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * This class will initialize the Validatior from Illuminate.
- * @todo Encapsulate it
  *
  * @package NunoLopes\LaravelContactsAPI
  */
 abstract class AbstractValidationRequest {
-    public $validator = null;
 
+    /**
+     * @var Validator $validator - Class to validate form data.
+     */
+    protected $validator = null;
+
+    /**
+     * @var Request $request - Class to retrieve form data.
+     */
+    protected $request = null;
+
+    /**
+     * AbstractValidationRequest constructor.
+     *
+     * @throws \LogicException
+     */
     public function __construct()
     {
+        // Initializes the request class.
+        $this->request = Request::createFromGlobals();
+
         // Initializes Validator's dependencies.
         $fileSystem = new Filesystem();
         $fileLoader = new FileLoader($fileSystem, 'lang');
@@ -25,15 +42,28 @@ abstract class AbstractValidationRequest {
         // Returns initialized instance.
         $this->validator = new Validator(
             $translator,
-            \array_merge(
-                $_GET,
-                $_POST,
-                $_FILES
-            ),
+            \json_decode($this->request->getContent(), true),
             $this->rules(),
             []
         );
     }
 
+    /**
+     * Returns all validated fields.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     *
+     * @return array
+     */
+    public function validated(): array
+    {
+        return $this->validator->validated();
+    }
+
+    /**
+     * All extending classes should have rules.
+     *
+     * @return array
+     */
     public abstract function rules(): array;
 }
