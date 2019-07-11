9da73583caa2e6ede4c87f089e0d569806b578e6
Nuno Lopes <nuno@redweb.dk>
2019-07-11 11:19:00 +0100

    Created Authentication Contract and its Utility to be injected from the application layer

---
+++ src/Contracts/Utilities/Authentication.php
@@ -0,0 +1,29 @@
+<?php
+namespace NunoLopes\LaravelContactsAPI\Contracts\Utilities;
+
+use NunoLopes\LaravelContactsAPI\Entities\User;
+
+/**
+ * Authentication Contract.
+ *
+ * This contract will allow other Authentication to be used with a Strategy Pattern, and
+ * making the code more SOLID (Dependency inversion principle).
+ *
+ * All Authentications Utilities will implement this Contract.
+ */
+interface Authentication
+{
+    /**
+     * Returns the authenticated User.
+     *
+     * @return User
+     */
+    public function user(): User;
+
+    /**
+     * Checks if the user is a guest.
+     *
+     * @return bool
+     */
+    public function guest(): bool;
+}
---
+++ src/Utilities/LaravelAuthentication.php
@@ -0,0 +1,36 @@
+<?php
+namespace NunoLopes\LaravelContactsAPI\Utilities;
+
+use Illuminate\Support\Facades\Auth;
+use NunoLopes\LaravelContactsAPI\Contracts\Utilities\Authentication;
+use NunoLopes\LaravelContactsAPI\Entities\User;
+
+/**
+ * Created by PhpStorm.
+ * User: nuno
+ * Date: 11-07-2019
+ * Time: 10:53
+ */
+class LaravelAuthentication implements Authentication
+{
+    /**
+     * @var Auth $auth - Authentication Manager.
+     */
+    protected $auth = null;
+
+    /**
+     * @inheritdoc
+     */
+    public function user(): User
+    {
+        return new User(Auth::user()->getAttributes());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function guest(): bool
+    {
+        return Auth::guest();
+    }
+}
