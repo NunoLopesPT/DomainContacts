9411d147597c5a48c71cd398d631419b949f2c34
Nuno Lopes <nuno@redweb.dk>
2019-07-13 22:52:49 +0100

    Created Abstract Entity

---
+++ src/Entities/AbstractEntity.php
@@ -0,0 +1,94 @@
+<?php
+namespace NunoLopes\LaravelContactsAPI\Entities;
+
+/**
+ * Abstract class AbstractEntity..
+ *
+ * @package NunoLopes\LaravelContactsAPI\Entities
+ */
+abstract class AbstractEntity
+{
+    /**
+     * Id of the entity.
+     *
+     * @var int
+     */
+    protected $id = null;
+
+    /**
+     * Contact constructor.
+     *
+     * @param $attributes
+     */
+    public function __construct($attributes)
+    {
+        $this->setAttributes($attributes);
+    }
+
+    /**
+     * Sets the attributes of the contact.
+     *
+     * @param $attributes
+     *
+     * @return void
+     */
+    public function setAttributes($attributes): void
+    {
+        foreach ($attributes as $key => $value) {
+
+            $setter = 'set' . str_replace('_', '', \ucwords($key, '_'));
+
+            if (\method_exists($this, $setter)) {
+                $this->{$setter}($value);
+            }
+        }
+    }
+
+    /**
+     * Returns the Entity's Attributes.
+     *
+     * @return array
+     */
+    public function getAttributes(): array
+    {
+        return \get_object_vars($this);
+    }
+
+    /**
+     * Returns the id of the Entity.
+     *
+     * @return int
+     */
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the id of the Entity.
+     *
+     * @param int $id - Sets the id of the Entity.
+     *
+     * @throws \InvalidArgumentException - If the id is not a positive number.
+     *
+     * @return void
+     */
+    protected function setId(int $id): void
+    {
+        if ($id <= 0) {
+            throw new \InvalidArgumentException('The id should be a positive number.');
+        }
+
+        $this->id = $id;
+    }
+
+    /**
+     * Transforms the Entity's attributes array to json.
+     *
+     * @return string
+     */
+    public function jsonSerialize(): string
+    {
+        return \json_encode($this->getAttributes());
+    }
+}
