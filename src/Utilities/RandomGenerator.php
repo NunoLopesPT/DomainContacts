cd6dd18833d240167f44397cd1658bd45807be7d
Nuno Lopes <nuno@redweb.dk>
2019-08-04 18:21:03 +0100

    Removed unused line

---
+++ src/Utilities/RandomGenerator.php
@@ -0,0 +1,26 @@
+<?php
+namespace NunoLopes\LaravelContactsAPI\Utilities;
+
+/**
+ * Class RandomGenerator.
+ *
+ * This class is used to generate random variables.
+ *
+ * @package NunoLopes\LaravelContactsAPI
+ */
+class RandomGenerator
+{
+    /**
+     * Renders a random string with a given length.
+     *
+     * @param int $length - Length of the string.
+     *
+     * @throws \Exception - If it was not possible to gather sufficient entropy.
+     *
+     * @return string
+     */
+    public static function string(int $length = 40): string
+    {
+        return \bin2hex(\random_bytes($length));
+    }
+}
