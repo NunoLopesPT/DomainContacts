cb0a77c11ab2465650eea509a9da419dc102b437
Nuno Lopes <nuno@redweb.dk>
2019-08-04 18:47:07 +0100

    Created ContactNotFound Exception

---
+++ src/Exceptions/Contacts/ContactNotFound.php
@@ -0,0 +1,14 @@
+<?php
+namespace NunoLopes\LaravelContactsAPI\Exceptions\Contacts;
+
+use NunoLopes\LaravelContactsAPI\Exceptions\NotFoundException;
+
+/**
+ * Class ContactNotFound.
+ *
+ * @package NunoLopes\LaravelContactsAPI
+ */
+class ContactNotFound extends NotFoundException
+{
+    protected $message = "Contact could not be found.";
+}
