7679487a4971d2a3f8f3580090ca88d5216e761b
Nuno Lopes <nuno@redweb.dk>
2019-08-19 19:56:04 +0100

    Created AccessTokenService

---
+++ src/Services/AccessTokenService.php
@@ -0,0 +1,115 @@
+<?php
+namespace NunoLopes\DomainContacts\Services;
+
+use NunoLopes\DomainContacts\Contracts\Database\AccessTokenRepository;
+use NunoLopes\DomainContacts\Contracts\Services\AuthenticationTokenService;
+use NunoLopes\DomainContacts\Entities\AccessToken;
+use NunoLopes\DomainContacts\Entities\User;
+use NunoLopes\DomainContacts\Exceptions\Authentication\TokenRevokedException;
+use NunoLopes\DomainContacts\Utilities\RandomGenerator;
+
+/**
+ * This Domain Service will be responsible for all Business Logic related with Access Tokens.
+ *
+ * @package NunoLopes\DomainContacts
+ */
+class AccessTokenService
+{
+    /**
+     * @var AccessTokenRepository $accessTokenRepository - AccessToken's Repository instance.
+     */
+    private $accessTokenRepository = null;
+
+    /**
+     * @var AuthenticationTokenService $authTokenService - Authentication Token Service Instance.
+     */
+    private $authTokenService = null;
+
+    /**
+     * AuthenticationService constructor.
+     *
+     * @param AccessTokenRepository      $accessTokenRepository - AccessToken's Repository instance.
+     * @param AuthenticationTokenService $authTokenService      - JWT Service Instance.
+     */
+    public function __construct(
+        AccessTokenRepository $accessTokenRepository,
+        AuthenticationTokenService $authTokenService
+    ) {
+        $this->accessTokenRepository = $accessTokenRepository;
+        $this->authTokenService      = $authTokenService;
+    }
+
+    /**
+     * Creates an Authentication Token.
+     *
+     * @param User $user - The user that the token will be created for.
+     *
+     * @throws \Exception - If was not possible to create a random string.
+     *
+     * @return string
+     */
+    public function createToken(User $user): string
+    {
+        // Collects current timestamp.
+        $stamp = new \DateTime();
+
+        // Creates a new AccessToken to be saved in the persistent layer.
+        $accessToken = new AccessToken([
+            'token_id'   => RandomGenerator::string(),
+            'user_id'    => $user->id(),
+            'revoked'    => false,
+            'created_at' => $stamp->format('Y-m-d H:i:s'),
+            'expires_at' => $stamp->add(new \DateInterval("P30D"))->format('Y-m-d H:i:s'),
+        ]);
+
+        // Saves the Token ID in the persistent layer.
+        $this->accessTokenRepository->create($accessToken);
+
+        // After saving the token in the database, create an authentication token.
+        return $this->authTokenService->create($accessToken);
+    }
+
+    /**
+     * Validates and returns an User from a given AuthenticationToken.
+     *
+     * @param string $authToken - Authentication Token.
+     *
+     * @throws TokenRevokedException - If the token is revoked.
+     *
+     * @return User
+     */
+    public function getTokenUser(string $authToken): User
+    {
+        // Decodes the Token, check its integrity and get the ID of the AccessToken.
+        $id = $this->authTokenService->accessTokenId($authToken);
+
+        // Find the token in the database.
+        $accessToken = $this->accessTokenRepository->findByToken($id);
+
+        // Check if the token is revoked.
+        if ($accessToken->revoked()) {
+            throw new TokenRevokedException();
+        }
+
+        // Return the User instance.
+        return $accessToken->user();
+    }
+
+    /**
+     * Revokes a token, checking its integrity first.
+     *
+     * Returns the successful of the operation.
+     *
+     * @param string $token - The authentication token.
+     *
+     * @return bool
+     */
+    public function revokeToken(string $token): bool
+    {
+        // Decodes the Token, check its integrity and get the ID of the AccessToken.
+        $id = $this->authTokenService->accessTokenId($token);
+
+        // Returns the success of the revoke operation.
+        return $this->accessTokenRepository->revoke($id);
+    }
+}
