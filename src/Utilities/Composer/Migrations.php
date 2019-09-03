4ee637b73ccb4ba8a78b7796425865cd169e7497
Nuno Lopes <nuno@redweb.dk>
2019-09-03 19:48:23 +0100

    Created Migrations Utility class to run migrations from composer

---
+++ src/Utilities/Composer/Migrations.php
@@ -0,0 +1,37 @@
+<?php
+namespace NunoLopes\DomainContacts\Utilities\Composer;
+
+use NunoLopes\DomainContacts\Factories\Services\MigrationsServiceFactory;
+
+/**
+ * Class Migrations.
+ *
+ * Utility class to help running migrations via composer script.
+ *
+ * @package NunoLopes\DomainContacts
+ */
+class Migrations
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public static function run(): void
+    {
+        // Fix this helper is not autoloaded.
+        require_once (__DIR__ . '/../../../vendor/illuminate/support/helpers.php');
+
+        MigrationsServiceFactory::get()->runMigrations();
+    }
+
+    /**
+     * Rollback migrations.
+     *
+     * @return void
+     */
+    public static function rollback(): void
+    {
+        MigrationsServiceFactory::get()->rollbackMigrations();
+    }
+}
