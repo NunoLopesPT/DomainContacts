21559d2b45a354876430cd7c370224b01888ef91
Nuno Lopes <nuno@redweb.dk>
2019-09-03 13:36:19 +0100

    Created Abstract Migration file

---
+++ src/Utilities/Database/Migrations/AbstractMigration.php
@@ -0,0 +1,29 @@
+<?php
+namespace NunoLopes\DomainContacts\Utilities\Database\Migrations;
+
+use Illuminate\Database\Schema\Builder;
+use NunoLopes\DomainContacts\Factories\Database\Eloquent\CapsuleFactory;
+
+/**
+ * Class AbstractMigration.
+ *
+ * This class will provide a schema for the migrations to run, all of them
+ * should extend this class.
+ *
+ * @package NunoLopes\DomainContacts
+ */
+abstract class AbstractMigration
+{
+    /**
+     * @var Builder $schema - Database schema instance.
+     */
+    protected $schema = null;
+
+    /**
+     * AbstractMigration constructor.
+     */
+    public function __construct()
+    {
+        $this->schema = CapsuleFactory::get()->schema();
+    }
+}
