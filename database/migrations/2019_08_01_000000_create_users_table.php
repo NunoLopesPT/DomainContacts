7cfbde43de785e71ec449fccb18e69eb33dfdad6
Nuno Lopes <nuno@redweb.dk>
2019-09-03 13:19:55 +0100

    Improved Users table

---
+++ database/migrations/2019_08_01_000000_create_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use NunoLopes\DomainContacts\Utilities\Database\Migrations\AbstractMigration;
+
+/**
+ * Class CreateUsersTable.
+ *
+ * This class will create the Users table.
+ *
+ * @package NunoLopes\DomainContacts
+ */
+class CreateUsersTable extends AbstractMigration
+{
+    /**
+     * @var string TABLE_NAME - The name of the table that is going to be created.
+     */
+    const TABLE_NAME = 'users';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Create table.
+        $this->schema->create(self::TABLE_NAME, function (Blueprint $table) {
+
+            // Add table columns.
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')
+                  ->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->dropIfExists(self::TABLE_NAME);
+    }
+}
