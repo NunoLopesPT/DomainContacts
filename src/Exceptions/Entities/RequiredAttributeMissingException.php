5c66eefdbc81f50cf4eb2fe178d832f748269726
Nuno Lopes <nuno@redweb.dk>
2019-09-12 11:15:16 +0100

    Created RequiredAttributeMissingException

---
+++ src/Exceptions/Entities/RequiredAttributeMissingException.php
@@ -0,0 +1,29 @@
+<?php
+namespace NunoLopes\DomainContacts\Exceptions\Contacts;
+
+use NunoLopes\DomainContacts\Exceptions\BaseException;
+
+/**
+ * Class RequiredAttributeMissingException.
+ *
+ * @package NunoLopes\LaravelContactsAPI
+ */
+class RequiredAttributeMissingException extends BaseException
+{
+    /**
+     * @var string $message - Exception message.
+     */
+    protected $message = "Required attribute (%s) is missing.";
+
+    /**
+     * RequiredAttributeMissing constructor.
+     *
+     * @param string $attribute - Name of the attribute that is missing.
+     */
+    public function __construct(string $attribute)
+    {
+        $this->message = \sprintf($this->message, $attribute);
+
+        parent::__construct();
+    }
+}
