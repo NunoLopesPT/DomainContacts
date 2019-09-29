91b711e299cf6ae8b5779cc830f00f4567cf5d21
Nuno Lopes <nuno@redweb.dk>
2019-09-29 18:33:48 +0100

    Created Migrations Factory

---
+++ src/Factories/Database/Eloquent/MigrationsFactory.php
@@ -0,0 +1,72 @@
+<?php
+namespace NunoLopes\DomainContacts\Factories\Database\Eloquent;
+
+use Illuminate\Database\ConnectionResolver;
+use Illuminate\Database\Migrations\DatabaseMigrationRepository;
+use Illuminate\Database\Migrations\Migrator;
+use Illuminate\Filesystem\Filesystem;
+
+/**
+ * Class MigrationsFactory.
+ *
+ * This class will create a singleton instance to deal with the migrations.
+ *
+ * @package NunoLopes\DomainContacts
+ */
+class MigrationsFactory
+{
+    /**
+     * @var $instance Migrator - Singleton instance of Migrator.
+     */
+    private static $instance = null;
+
+    /**
+     * Returns a new instance of Migrator.
+     *
+     * @return Migrator
+     */
+    private static function create(): Migrator
+    {
+        // Creates the resolver with a default connection to the database.
+        $resolver = new ConnectionResolver([
+            'default' => CapsuleFactory::get()->getConnection()
+        ]);
+
+        // Starts the DatabaseMigrations repository with the
+        // migrations table
+        $repository = new DatabaseMigrationRepository(
+            $resolver,
+            'migrations'
+        );
+
+        // Set the database connection default to use in the repository.
+        $repository->setSource('default');
+
+        // Start the Migrator.
+        $migrator = new Migrator(
+            $repository,
+            $resolver,
+            new Filesystem()
+        );
+
+        // Set the database connection default to use in the repository.
+        $migrator->setConnection('default');
+
+        return $migrator;
+    }
+
+    /**
+     * Get a new Migrator instance if not found or
+     * return the one already created (Singleton).
+     *
+     * @return Migrator
+     */
+    public static function get(): Migrator
+    {
+        if (self::$instance === null) {
+            self::$instance = self::create();
+        }
+
+        return self::$instance;
+    }
+}
