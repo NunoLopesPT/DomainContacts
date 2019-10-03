5b70cef103f94f91c0c3a32ded7db57ce69dd709
Nuno Lopes <nuno@redweb.dk>
2019-10-03 12:35:35 +0100

    Added Create Function for a Signature

--- src/Utilities/Signatures/Sha256RsaSignature.php
+++ src/Utilities/Signatures/Sha256RsaSignature.php
@@ -1,7 +1,8 @@
 <?php
-namespace NunoLopes\DomainContacts\Utilities;
+namespace NunoLopes\DomainContacts\Utilities\Signatures;
 
 use NunoLopes\DomainContacts\Contracts\Utilities\RsaSignature;
+use NunoLopes\DomainContacts\Datatypes\AsymmetricCryptography;
 
 /**
  * Class Sha256Signature.
@@ -23,6 +24,36 @@ class Sha256RsaSignature implements RsaSignature
     public function code(): string
     {
         return self::CODE;
+    }
+
+    /**
+     * Creates a new KeyPair and returns the private and the public
+     * key in a DataType.
+     *
+     * @return AsymmetricCryptography
+     */
+    public function create(): AsymmetricCryptography
+    {
+        $config = array(
+            "digest_alg"       => "sha256",
+            "private_key_bits" => 2048,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        );
+
+        // Create the private and public key
+        $res = openssl_pkey_new($config);
+
+        // Extract the private key from $res to $privKey
+        openssl_pkey_export($res, $privKey);
+
+        // Extract the public key from $res to $pubKey
+        $pubKey = openssl_pkey_get_details($res);
+        $pubKey = $pubKey["key"];
+
+        return new AsymmetricCryptography(
+            $pubKey,
+            $privKey
+        );
     }
 
     /**
