8a6920641715a1576b4f3bfc5810b53317a51e68
Nuno Lopes <nuno@redweb.dk>
2019-10-03 20:40:57 +0100

    Added generateKeyPair

--- src/Utilities/Composer/RunScripts.php
+++ src/Utilities/Composer/RunScripts.php
@@ -3,12 +3,14 @@ namespace NunoLopes\DomainContacts\Utilities\Composer;
 
 use NunoLopes\DomainContacts\Factories\Services\Database\MigrationsServiceFactory;
 use NunoLopes\DomainContacts\Factories\Services\Database\SeedsServiceFactory;
+use NunoLopes\DomainContacts\Utilities\Signatures\Sha256RsaSignature;
 
 /**
  * Class Migrations.
  *
  * Utility class to help running migrations via composer script.
  *
+ * @codeCoverageIgnore
  * @package NunoLopes\DomainContacts
  */
 class RunScripts
@@ -48,5 +50,26 @@ class RunScripts
         require_once (__DIR__ . '/../../../vendor/illuminate/support/helpers.php');
 
         MigrationsServiceFactory::get()->rollbackMigrations();
+    }
+
+    /**
+     * Generates a new KeyPair in the storage folder.
+     *
+     * @return void
+     */
+    public static function generateKeyPair(): void
+    {
+        $sign    = new Sha256RsaSignature();
+        $keyPair = $sign->create();
+
+        // Store the private and the public key in the filesystem.
+        \file_put_contents(
+            __DIR__ . '/../../../storage/oauth-private.key',
+            $keyPair->privateKey()
+        );
+        \file_put_contents(
+            __DIR__ . '/../../../storage/oauth-public.key',
+            $keyPair->publicKey()
+        );
     }
 }
