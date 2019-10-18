0c9979e24143ab43758fa3ef593b4de89caa33ec
Nuno Lopes <nuno@redweb.dk>
2019-10-18 11:58:18 +0100

    Refactor Update Request

--- src/Requests/Contacts/UpdateContactRequest.php
+++ src/Requests/Contacts/UpdateContactRequest.php
@@ -1,30 +1,11 @@
 <?php
 namespace NunoLopes\DomainContacts\Requests\Contacts;
 
-use NunoLopes\DomainContacts\Requests\AbstractValidationRequest;
-
 /**
  * This class will define the rules for all UpdateContactRequest.
  *
- * @todo this request is similar to the create one, try to refactor this.
- *
  * @package NunoLopes\DomainContacts
  */
-class UpdateContactRequest extends AbstractValidationRequest
+class UpdateContactRequest extends CreateContactRequest
 {
-    /**
-     * The rules() method will return an array containing
-     * all the rules to be validated by the Request object.
-     *
-     * @return array
-     */
-    public function rules(): array
-    {
-        return [
-            'first_name'   => 'required|min:3|max:255',
-            'last_name'    => 'max:255',
-            'email'        => 'nullable|regex:/\S+@\S+\.\S+/i|max:255',
-            'phone_number' => 'nullable|string|max:255',
-        ];
-    }
 }
